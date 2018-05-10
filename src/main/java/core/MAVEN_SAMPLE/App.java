fca6f3c49ae7ac4314498c18de6e74545a77ae8d
Serega <serv3546@gmail.com>
2018-05-09 20:01:34 -0700

    init

---
+++ src/main/java/core/MAVEN_SAMPLE/App.java
@@ -0,0 +1,13 @@
+package core.MAVEN_SAMPLE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
